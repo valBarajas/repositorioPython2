230ac166164b53f43241cc5492feec2f5f4b6e30
Violentin <victor.z.barajas@gmail.com>
2022-10-03 13:44:34 -0500

    5

---
+++ diccionario.py
@@ -0,0 +1,10 @@
+dictionary = {
+              "fulanito":87,
+              "ximena":98,
+              "cesar":80,
+              "rodrigo":100
+              }
+for key in dictionary.keys():
+    print(key,"your kkdkdkd is",dictionary[key])
+
+#investigar de tuplas y arreglos , key
